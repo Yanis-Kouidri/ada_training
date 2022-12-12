6b03c9e27503f979441aa2d094dbbfa60caa1c7e
Yanis Kouidri <yanis.kouidri@gmail.com>
2022-12-12 08:10:25 +0100

    Modification template

--- template.adb
+++ template.adb
@@ -1,14 +1,14 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 procedure template is
-    m: Integer;
+    -- Déclaration de types
+    
+
+    -- Déclaration de procédures et fonctions
+    
+    
+    -- Déclaration de variables
 begin
-    m := 0;
-    Put("Donner m : ");
-    Get(m);
-    new_line;
-    for i in 1..9 loop
-        m := m + i;
-    end loop;
-    Put_line("m : " & Integer'Image(m));
+
+
 end template; 
