aac0cac12c9242f57009bb88b82f2826c825ef61
Yanis Kouidri <yanis.kouidri@gmail.com>
2022-12-27 13:09:46 +0100

    Améliorations d'affichage de tri par insertion

--- Exercices_sur_les_tableaux/tri_par_insertion.adb
+++ Exercices_sur_les_tableaux/tri_par_insertion.adb
@@ -1,17 +1,18 @@
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 procedure tri_par_insertion is
-    NMAX : constant Integer := 8; --indice maximum du tableau
+    NMAX : constant Integer := 11; --indice maximum du tableau
     Type TAB_ENTIERS is Array(1..NMAX) of Integer;
     un_tab : TAB_ENTIERS; -- un tableau d'au maximum NMAX entiers
     buffer: Integer := 0;
 begin
-    un_tab := (39,13,74,8,26,90,35,4); --Tableau non trié
-    for i in 1..NMAX loop
+    un_tab := (1,18,74,8,2,90,35,4, 345, 4, 5); --Tableau quelconque non trié
+    put_line("Tableau non trié :");
+    for i in 1..NMAX loop -- Affichage du tableau non trié :
         put(un_tab(i));
     end loop;
     
-    for i in 1..NMAX-1 loop
+    for i in 1..NMAX-1 loop -- Tri du tableau par insertion
         for j in 1..i loop
 
             if un_tab(j) > un_tab(i+1) then
@@ -21,11 +22,12 @@ begin
             end if;
 
         end loop;
-
     end loop;
-
+    
     new_line;
-    for i in 1..NMAX loop
+
+    put_line("Tableau trié :");
+    for i in 1..NMAX loop -- Affichage du tableau trié
         put(un_tab(i));
     end loop;
 
