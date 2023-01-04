c6be937ba2db186b232c9c0f1b35606f2c0e801d
Yanis Kouidri <yanis.kouidri@gmail.com>
2023-01-04 19:16:35 +0100

    Fin iteratif au recursif

--- Iteratif_et_recursif/iteratif_au_recursif.adb
+++ Iteratif_et_recursif/iteratif_au_recursif.adb
@@ -17,13 +17,15 @@ procedure iteratif_au_recursif is
         end loop;
     end affiche;
 
-    procedure affiche_rec(ligne, colone : in integer) is
+    procedure affiche_rec(ligne, colone : in integer) is -- Meme fonction mais écrite de manière récursive
     begin
-        if ligne > 0 then
-            put(colone);
-            affiche_rec(ligne - 1, colone - 1);
-        else
-            put(colone);
+        if ligne >= 0 then
+            affiche_rec(ligne - 1, colone);
+            for b in 0..colone loop
+            -- afficher un nombre
+                put(10*ligne + b);
+            end loop;
+            new_line;
         end if;
 
     end affiche_rec;
@@ -31,7 +33,12 @@ procedure iteratif_au_recursif is
     
     -- Déclaration de variables
 begin
-    affiche_rec(3,5);
+    Put_line("Itératif :");
+    affiche(3,5);
 
+    new_line;
+
+    Put_line("Récursif :");
+    affiche_rec(3,5);
 
 end iteratif_au_recursif; 
